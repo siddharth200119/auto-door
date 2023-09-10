b08a20472a60475726edd96c9e96ba07ab6f580a
siddharth200119 <siddramnani@gmail.com>
2023-09-10 15:29:02 +0530

    connected esp32 to wifi

---
+++ ESP32_configure/ESP32_configure.ino
@@ -0,0 +1,28 @@
+#include <WiFi.h>
+
+const char* ssid = "Stain";
+const char* password = "b$VJ518175";
+
+void setup() {
+   Serial.begin(115200);
+   delay(1000);
+
+   WiFi.mode(WIFI_STA); //Optional
+   WiFi.begin(ssid, password);
+   Serial.println("\nConnecting");
+
+   while(WiFi.status() != WL_CONNECTED){
+       Serial.print(".");
+       delay(100);
+   }
+
+   Serial.println("\nConnected to the WiFi network");
+   Serial.print("Local ESP32 IP: ");
+   Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
