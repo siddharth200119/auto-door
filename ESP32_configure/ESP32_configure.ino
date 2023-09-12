1c3ba3442ae3a667f3fa0a0e24f2a8207b839c78
siddharth200119 <siddramnani@gmail.com>
2023-09-13 00:07:30 +0530

    can read PIR data now

--- ESP32_configure/ESP32_configure.ino
+++ ESP32_configure/ESP32_configure.ino
@@ -4,9 +4,12 @@
 
 const char* ssid = "Stain";
 const char* password = "b$VJ518175";
+
 char* door_status = "open";
+int sensor_output;
 
 static const int servoPin = 3;
+static const int pirPin = 2;
 
 WebServer server(80);
 Servo MyServo;
@@ -15,11 +18,21 @@ void setup_routing() {
   server.on("/close_door", close_door);     
   server.on("/open_door", open_door);     
   server.on("/door_status", getDoor_status);         
-  //server.on("/motion_sensor_data", HTTP_POST, handlePost);           
+  server.on("/motion_sensor_data", PIR_data);           
   server.begin();    
 }
 
 //dummy functions
+
+void PIR_data(){
+    sensor_output = digitalRead(pirPin);
+    if(sensor_output == LOW){
+        server.send(200, "text/plain", "No person detected");
+      }
+    else{
+        server.send(200, "text/plain", "person detected");
+      }
+  }
 
 void close_door(){
     Serial.println("Close Door");
@@ -36,11 +49,13 @@ void open_door(){
 void getDoor_status(){
     Serial.println("Get Door Status");
     server.send(200, "text/plain", door_status);
+    MyServo.write(60);
   }
 
 void setup() {
    Serial.begin(115200);
    MyServo.attach(servoPin);
+   pinMode(pirPin, INPUT);
    delay(1000);
 
    WiFi.mode(WIFI_STA); //Optional
