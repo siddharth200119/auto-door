22663d0dde21e25ede4bcb2a1640ed602a209cc6
siddharth200119 <siddramnani@gmail.com>
2023-09-30 15:52:52 +0530

    made changes to server code

--- test_server_raspberryPI/server.py
+++ test_server_raspberryPI/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
+import sys
 
 app = Flask(__name__)
 
 @app.route('/sidd_room/motion_detection', methods=['POST'])
 def motion_detection():
     if request.method == 'POST':
-        print(request.json)
+        print(request.json, file=sys.stderr)
+        return(request.json)
 
 if __name__ =='__main__':  
     app.run(port = 6969) 
